57c6b2ccdf7fcc4346aa3bd6c8f77a32cfaf6307
Michael Randrianarisona <michael.randrianarisona@versusmind.eu>
2023-03-21 02:46:25 +0300

    refactor: remove PhotoEvent props equatable as it is useless

--- lib/photos/bloc/photo_event.dart
+++ lib/photos/bloc/photo_event.dart
@@ -4,8 +4,6 @@ part of 'photo_bloc.dart';
 @immutable
 abstract class PhotoEvent extends Equatable {
   const PhotoEvent();
-  @override
-  List<Object> get props => [];
 }
 
 class PhotoFetched extends PhotoEvent {
